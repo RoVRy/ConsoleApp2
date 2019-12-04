e38567526c2123e8e8cbad44d267f44af9e9e697
Rex2701 <58503026+Rex2701@users.noreply.github.com>
2019-12-04 21:05:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        Encoding ascii = Encoding.ASCII;
+        static void Main(string[] args)
+        {
+            string str;
+            if (args.Length ==0) for (; ; )
+                {
+                    Console.Write("Исходная строка (<Enter> для выхода): ");
+                    str = Console.ReadLine();
+                    if (str == "") break;
+                    Console.WriteLine("{0}", ToCodes(str));
+                }
+            else Console.WriteLine("{0}", ToCodes(args[0]));
+        }
+
+        static string ToCodes(string p)
+        {
+            int i; string str="";
+            for (i = 0; i < p.Length; i++)
+            {
+                str += Convert.ToByte(p[i]) + " ";
+            }
+            return "Коды символов: " + str + "\n";
+        }
+    }
+}
